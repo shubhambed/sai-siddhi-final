0ab114bfc24f3912d5f41f9551dc73bc43016e59
shubhambed <156799287+shubhambed@users.noreply.github.com>
2025-12-30 16:36:21 +0530

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,8 @@
+
+window.addEventListener('scroll',()=>{
+document.querySelectorAll('.reveal').forEach(el=>{
+if(el.getBoundingClientRect().top < window.innerHeight-100){
+el.classList.add('active');
+}
+});
+});
