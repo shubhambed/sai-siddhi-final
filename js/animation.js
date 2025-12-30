79ae71c9456f3002c4438b925e3d394cf8ea8e7f
shubhambed <156799287+shubhambed@users.noreply.github.com>
2025-12-30 16:45:27 +0530

    Create animation.js

---
+++ js/animation.js
@@ -0,0 +1,9 @@
+
+window.addEventListener('scroll',()=>{
+document.querySelectorAll('.reveal').forEach(el=>{
+if(el.getBoundingClientRect().top < window.innerHeight-100){
+el.classList.add('active');
+}
+});
+});
+
