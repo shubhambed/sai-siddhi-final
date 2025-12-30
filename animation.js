0110c221ee5c22ee9ad57faf9096c666c31e6cee
shubhambed <156799287+shubhambed@users.noreply.github.com>
2025-12-30 20:07:44 +0530

    Delete animation.js

--- animation.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("scroll", () => {
-  document.querySelectorAll(".card").forEach(card => {
-    const pos = card.getBoundingClientRect().top;
-    if (pos < window.innerHeight - 100) {
-      card.style.opacity = "1";
-      card.style.transform = "translateY(0)";
-    }
-  });
-});
